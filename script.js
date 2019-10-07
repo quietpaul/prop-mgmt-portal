948e448e7fde9ef1503e5672213d102bce824ccc
Paul Woodworth <35321164+quietpaul@users.noreply.github.com>
2019-10-07 11:04:10 -0400

    Upload
    Initial upload of first draft files.

---
+++ script.js
@@ -0,0 +1,37 @@
+var usernames = [
+	{
+
+		// if(username.isTrue == true){
+		// 	// allow into site.
+
+		// 	} else {
+		// 	return = "incorrect username"
+		// 	}
+
+
+
+
+
+	},
+	{}
+
+]
+
+
+
+
+var tenants = [
+	{
+		name: "Jack Johnson",
+		address: "399 Blackstone St #31",
+		unit: "2"
+		monthsInResidence: function(){
+				// insert code for counter
+			}
+		leaseDuration: // also insert code
+		monthlyRent:
+		totalRentDue:
+		balanceDue:
+	}
+]
+
